04bbf7fb06997b368687b272866767b979c78efa
Joffrey BION <joffrey.bion@gmail.com>
2016-12-08 02:49:52 +0100

    Add initial webapp files to satisfy Travis for a Spring Boot build

---
+++ src/main/java/org/luxons/sevenwonders/app/SevenWonders.java
@@ -0,0 +1,12 @@
+package org.luxons.sevenwonders.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenWonders {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenWonders.class, args);
+    }
+}
