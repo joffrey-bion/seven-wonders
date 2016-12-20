552acb769250685764a673670d7828c38efa01d0
Victor Chabbert <chabbertvi@eisti.eu>
2016-12-20 15:39:44 +0100

    Disable stomp debug in production

--- src/main/js/src/utils/createWebSocketConnection.js
+++ src/main/js/src/utils/createWebSocketConnection.js
@@ -3,7 +3,9 @@ import Stomp from 'webstomp-client'
 const wsURL = 'http://localhost:8080/seven-wonders-websocket'
 
 const createConnection = (headers = {}) => new Promise((resolve, reject) => {
-    let socket = Stomp.over(new SockJS(wsURL))
+    let socket = Stomp.over(new SockJS(wsURL), {
+      debug: process.env.NODE_ENV !== "production"
+    })
     socket.connect(headers, (frame) => {
       return resolve({ frame, socket })
     }, reject)
