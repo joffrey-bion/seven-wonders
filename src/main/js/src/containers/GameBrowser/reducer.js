3ae4e0dc242b91d60a780eb5018f0d4884370128
Victor Chabbert <chabbertvi@eisti.eu>
2016-12-21 14:24:22 +0100

    Add immutable.js for data

--- src/main/js/src/containers/GameBrowser/reducer.js
+++ src/main/js/src/containers/GameBrowser/reducer.js
@@ -1,15 +1,12 @@
+import { Map } from 'immutable'
 import { NEW_GAME } from './constants'
 
-const initialState = {
-}
+const initialState = Map({})
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case NEW_GAME:
-      return {
-        ...state,
-        [action.game.id]: action.game
-      }
+      return Map.set(action.game.id, action.game)
     default:
       return state
   }
