5bbc7237b306da10c8d798cc9af22c8936b1b3c1
jbion <joffrey.bion@amadeus.com>
2016-12-21 22:12:06 +0100

    Add playerIndex and validation method to the Move itself

--- src/main/java/org/luxons/sevenwonders/game/Move.java
+++ src/main/java/org/luxons/sevenwonders/game/Move.java
@@ -3,15 +3,26 @@ package org.luxons.sevenwonders.game;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.luxons.sevenwonders.game.api.Table;
 import org.luxons.sevenwonders.game.resources.Resources;
 
 public class Move {
+
+    private int playerIndex;
 
     private String cardName;
 
     private MoveType moveType;
 
     private Map<Integer, Resources> boughtResources = new HashMap<>();
+
+    public int getPlayerIndex() {
+        return playerIndex;
+    }
+
+    public void setPlayerIndex(int playerIndex) {
+        this.playerIndex = playerIndex;
+    }
 
     public String getCardName() {
         return cardName;
@@ -35,5 +46,10 @@ public class Move {
 
     public void setBoughtResources(Map<Integer, Resources> boughtResources) {
         this.boughtResources = boughtResources;
+    }
+
+    public boolean isValid(Table table) {
+        // TODO
+        return false;
     }
 }
