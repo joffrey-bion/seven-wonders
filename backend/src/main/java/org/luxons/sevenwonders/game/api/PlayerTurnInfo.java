fc4be773629ec0961f78292d589237fd1d5d8ba8
Joffrey BION <joffrey.bion@gmail.com>
2017-01-22 17:25:54 +0100

    Remove handRotationDirection in PlayerTurnInfo
    It is already provided in the nested Table object.

--- backend/src/main/java/org/luxons/sevenwonders/game/api/PlayerTurnInfo.java
+++ backend/src/main/java/org/luxons/sevenwonders/game/api/PlayerTurnInfo.java
@@ -3,7 +3,6 @@ package org.luxons.sevenwonders.game.api;
 import java.util.List;
 
 import org.luxons.sevenwonders.game.Player;
-import org.luxons.sevenwonders.game.cards.HandRotationDirection;
 
 public class PlayerTurnInfo {
 
@@ -12,8 +11,6 @@ public class PlayerTurnInfo {
     private final Table table;
 
     private int currentAge;
-
-    private HandRotationDirection handRotationDirection;
 
     private Action action;
 
@@ -40,14 +37,6 @@ public class PlayerTurnInfo {
 
     public void setCurrentAge(int currentAge) {
         this.currentAge = currentAge;
-    }
-
-    public HandRotationDirection getHandRotationDirection() {
-        return handRotationDirection;
-    }
-
-    public void setHandRotationDirection(HandRotationDirection handRotationDirection) {
-        this.handRotationDirection = handRotationDirection;
     }
 
     public List<HandCard> getHand() {
