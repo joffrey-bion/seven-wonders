2de65567068db12c83bf6225ac0e971705f25d7b
jbion <joffrey.bion@amadeus.com>
2017-05-25 03:31:48 +0200

    Rework integration tests with new client and API

--- backend/src/test/java/org/luxons/sevenwonders/SevenWondersTest.java
+++ backend/src/test/java/org/luxons/sevenwonders/SevenWondersTest.java
@@ -1,27 +1,17 @@
 package org.luxons.sevenwonders;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.luxons.sevenwonders.actions.ChooseNameAction;
-import org.luxons.sevenwonders.test.ClientPlayer;
-import org.luxons.sevenwonders.test.TestStompFrameHandler;
-import org.luxons.sevenwonders.test.TestUtils;
+import org.luxons.sevenwonders.test.api.ApiLobby;
+import org.luxons.sevenwonders.test.api.SevenWondersSession;
+import org.luxons.sevenwonders.test.client.JsonStompClient;
+import org.luxons.sevenwonders.test.client.JsonStompSession;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSession.Subscription;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -29,39 +19,55 @@ public class SevenWondersTest {
 
     private static final String WEBSOCKET_URI = "ws://localhost:%d/seven-wonders-websocket";
 
-    private static WebSocketStompClient stompClient;
+    private static JsonStompClient client;
 
     @LocalServerPort
     private int randomServerPort;
 
     @BeforeClass
     public static void setUp() {
-        stompClient = TestUtils.createStompClient();
+        client = new JsonStompClient();
+    }
+
+    private SevenWondersSession connectNewUser() throws Exception {
+        JsonStompSession session = client.connect(String.format(WEBSOCKET_URI, randomServerPort));
+        return new SevenWondersSession(session);
+    }
+
+    private void disconnect(SevenWondersSession... sessions) {
+        for (SevenWondersSession session : sessions) {
+            session.disconnect();
+        }
     }
 
     @Test
     public void testConnection() throws Exception {
-        StompSession session = TestUtils.connect(stompClient, String.format(WEBSOCKET_URI, randomServerPort));
-        BlockingQueue<ClientPlayer> blockingQueue = new LinkedBlockingDeque<>();
-        Subscription sub = session.subscribe("/user/queue/nameChoice",
-                new TestStompFrameHandler<>(blockingQueue, ClientPlayer.class));
-
-        String testName = "Test User";
-        ChooseNameAction chooseNameAction = new ChooseNameAction();
-        chooseNameAction.setPlayerName(testName);
-
-        session.send("/app/chooseName", chooseNameAction);
-
-        ClientPlayer player = blockingQueue.poll(2, SECONDS);
-        assertNotNull(player);
-        assertEquals(testName, player.getDisplayName());
-
-        sub.unsubscribe();
+        SevenWondersSession session = connectNewUser();
+        session.chooseName("Test User");
         session.disconnect();
+    }
+
+    @Test
+    public void testConnection_2users() throws Exception {
+        SevenWondersSession session1 = connectNewUser();
+        SevenWondersSession session2 = connectNewUser();
+        session1.chooseName("Player1");
+        session2.chooseName( "Player2");
+
+        ApiLobby lobby = session1.createGame("Test Game");
+        session2.joinGame(lobby.getId());
+
+        SevenWondersSession session3 = connectNewUser();
+        session3.chooseName("Player3");
+        session3.joinGame(lobby.getId());
+
+        session1.startGame(lobby.getId());
+
+        disconnect(session1, session2, session3);
     }
 
     @AfterClass
     public static void tearDown() {
-        stompClient.stop();
+        client.stop();
     }
 }
