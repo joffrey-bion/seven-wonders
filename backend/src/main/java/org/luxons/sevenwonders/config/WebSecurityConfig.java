12169481b4adcceba54ca06999ec5a3613b96c19
Joffrey BION <joffrey.bion@gmail.com>
2018-04-28 10:34:50 +0200

    Migrate to Spring Boot Starter Security
    So far, we had a direct dependency on Spring Security Core, but it is cleaner to import it "à la" Spring Boot, so that we benefit from autoconfiguration, and the version is handled by Spring Boot.

---
+++ backend/src/main/java/org/luxons/sevenwonders/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package org.luxons.sevenwonders.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) {
+        // this disables default authentication settings
+    }
+}
