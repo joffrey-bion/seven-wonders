4c3c8f557a153552510f498d7d9755350982a7b2
Joffrey BION <joffrey.bion@gmail.com>
2018-06-09 13:56:53 +0200

    Refactor PlayerList

---
+++ frontend/src/components/PlayerList.jsx
@@ -0,0 +1,23 @@
+//@flow
+import { Text } from '@blueprintjs/core';
+import { List } from 'immutable';
+import React from 'react';
+import { Flex } from 'reflexbox';
+import { Player } from '../models/players';
+
+export type PlayerListProps = {
+  players: List<Player>;
+};
+
+const PlayerItem = ({player}) => (
+  <Flex>
+    <Text>{player.displayName}</Text>
+    <Text>({player.username})</Text>
+  </Flex>
+);
+
+export const PlayerList = ({players}: PlayerListProps) => (
+  <div>
+    {players.map(player => <PlayerItem key={player.username} player={player}/>)}
+  </div>
+);
