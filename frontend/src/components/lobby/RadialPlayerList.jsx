510c4cfeb666ab385e7200e1ac243fc6ed31043b
Joffrey BION <joffrey.bion@gmail.com>
2018-06-12 00:36:08 +0200

    Place current player always at the bottom

--- frontend/src/components/lobby/RadialPlayerList.jsx
+++ frontend/src/components/lobby/RadialPlayerList.jsx
@@ -21,7 +21,7 @@ const PlayerItem = ({player, isOwner, isUser}) => (
 );
 
 const PlayerPlaceholder = () => (
-  <Flex column align='center' style={{opacity: 0.4}}>
+  <Flex column align='center' style={{opacity: 0.3}}>
     <UserIcon isOwner={false} isUser={false}/>
     <h5 style={{margin: 0}}>?</h5>
   </Flex>
@@ -35,18 +35,26 @@ const UserIcon = ({isUser, isOwner}) => {
 };
 
 export const RadialPlayerList = ({players, owner, currentPlayer}: RadialPlayerListProps) => {
-  const playerItems = players.map(player => <PlayerItem key={player.username}
-                                                        player={player}
-                                                        isOwner={player.username === owner}
-                                                        isUser={player.username === currentPlayer.username}/>);
+  const orderedPlayers = placeFirst(players.toArray(), currentPlayer.username);
+  const playerItems = orderedPlayers.map(player => <PlayerItem key={player.username}
+                                                               player={player}
+                                                               isOwner={player.username === owner}
+                                                               isUser={player.username === currentPlayer.username}/>);
   const tableImg = <img src={roundTable} alt='Round table' style={{width: 200, height: 200}}/>;
-  return <RadialList items={completeWithPlaceholders(playerItems.toArray())}
+  return <RadialList items={completeWithPlaceholders(playerItems)}
                      centerElement={tableImg}
                      diameter={350}
                      offsetDegrees={180}
                      itemWidth={120}
                      itemHeight={100}/>;
 };
+
+function placeFirst(players: Array<Player>, targetFirstUsername: string): Array<Player> {
+  while (players[0].username !== targetFirstUsername) {
+    players.push(players.shift());
+  }
+  return players;
+}
 
 function completeWithPlaceholders(playerItems: Array<React.Node>): Array<React.Node> {
   while (playerItems.length < 3) {
