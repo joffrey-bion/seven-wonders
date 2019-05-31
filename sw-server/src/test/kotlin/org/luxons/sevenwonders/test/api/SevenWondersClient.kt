d61ea160f141e411f3940fcb3d0604490f6bd0a6
Joffrey BION <joffrey.bion@gmail.com>
2019-05-31 20:10:43 +0200

    Remove unnecessary Resources deserializer in test JacksonMapper

--- sw-server/src/test/kotlin/org/luxons/sevenwonders/test/api/SevenWondersClient.kt
+++ sw-server/src/test/kotlin/org/luxons/sevenwonders/test/api/SevenWondersClient.kt
@@ -1,11 +1,8 @@
 package org.luxons.sevenwonders.test.api
 
-import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.hildan.jackstomp.JackstompClient
 import org.luxons.sevenwonders.config.SEVEN_WONDERS_WS_ENDPOINT
-import org.luxons.sevenwonders.engine.resources.MutableResources
-import org.luxons.sevenwonders.engine.resources.Resources
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeoutException
@@ -15,11 +12,7 @@ class SevenWondersClient {
     private val client: JackstompClient
 
     init {
-        val customMappingsModule = SimpleModule("ConcreteResourcesDeserializationModule")
-        customMappingsModule.addAbstractTypeMapping(Resources::class.java, MutableResources::class.java)
-
         val mappingJackson2MessageConverter = MappingJackson2MessageConverter()
-        mappingJackson2MessageConverter.objectMapper.registerModule(customMappingsModule)
         mappingJackson2MessageConverter.objectMapper.registerModule(KotlinModule())
 
         client = JackstompClient()
