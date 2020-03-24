10447dded5583531075b14d20803bb32af02e630
Joffrey Bion <joffrey.bion@booking.com>
2020-03-24 11:48:33 +0100

    Enable redux dev tools

--- sw-ui-kt/src/main/kotlin/org/luxons/sevenwonders/ui/redux/Store.kt
+++ sw-ui-kt/src/main/kotlin/org/luxons/sevenwonders/ui/redux/Store.kt
@@ -5,16 +5,19 @@ import redux.RAction
 import redux.Store
 import redux.WrapperAction
 import redux.applyMiddleware
-import redux.compose
 import redux.createStore
 import redux.rEnhancer
+import kotlin.browser.window
 
 val INITIAL_STATE = SwState()
+
+private fun <A, T1, R> composeWithDevTools(function1: (T1) -> R, function2: (A) -> T1): (A) -> R =
+        window.asDynamic().__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(function1, function2) as Function1<A, R>
 
 fun configureStore(
     sagaManager: SagaManager<SwState, RAction, WrapperAction>,
     initialState: SwState = INITIAL_STATE
 ): Store<SwState, RAction, WrapperAction> {
     val sagaEnhancer = applyMiddleware(sagaManager.createMiddleware())
-    return createStore(::rootReducer, initialState, compose(sagaEnhancer, rEnhancer()))
+    return createStore(::rootReducer, initialState, composeWithDevTools(sagaEnhancer, rEnhancer()))
 }
