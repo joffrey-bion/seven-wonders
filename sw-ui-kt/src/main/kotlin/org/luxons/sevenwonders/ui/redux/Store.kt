7d4c12bc90a7496252d670966918b93e0c00a34a
Joffrey Bion <joffrey.bion@booking.com>
2020-03-26 20:39:42 +0100

    Use plain redux compose when devtools not present

--- sw-ui-kt/src/main/kotlin/org/luxons/sevenwonders/ui/redux/Store.kt
+++ sw-ui-kt/src/main/kotlin/org/luxons/sevenwonders/ui/redux/Store.kt
@@ -5,14 +5,20 @@ import redux.RAction
 import redux.Store
 import redux.WrapperAction
 import redux.applyMiddleware
+import redux.compose
 import redux.createStore
 import redux.rEnhancer
 import kotlin.browser.window
 
 val INITIAL_STATE = SwState()
 
-private fun <A, T1, R> composeWithDevTools(function1: (T1) -> R, function2: (A) -> T1): (A) -> R =
-        window.asDynamic().__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(function1, function2) as Function1<A, R>
+private fun <A, T1, R> composeWithDevTools(function1: (T1) -> R, function2: (A) -> T1): (A) -> R {
+    val reduxDevtoolsExtensionCompose = window.asDynamic().__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    if (reduxDevtoolsExtensionCompose == undefined) {
+        return compose(function1, function2)
+    }
+    return reduxDevtoolsExtensionCompose(function1, function2) as Function1<A, R>
+}
 
 fun configureStore(
     sagaManager: SagaManager<SwState, RAction, WrapperAction>,
