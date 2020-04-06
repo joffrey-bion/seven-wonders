d4d20533556928f63c8759437f67e76336bab55e
Joffrey Bion <joffrey.bion@booking.com>
2020-04-06 18:55:25 +0200

    Delete old React/TypeScript UI

--- sw-ui/src/redux/actions/all.ts
+++
@@ -1,5 +0,0 @@
-import { GameAction } from './game';
-import { LobbyAction } from './lobby';
-import { PlayerAction } from './user';
-
-export type Action = PlayerAction | LobbyAction | GameAction
--- sw-ui/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/seven-wonders-websocket', {
-    "target": "http://localhost:8080",
-    "ws": true
-  }));
-};
