ed68073e15e1042eb4b306fcfbe1417d35b92a7e
Joffrey Bion <joffrey.bion@booking.com>
2020-08-16 09:37:16 +0200

    Upgrade to Spring Boot 2.3.3

--- sw-server/build.gradle.kts
+++ sw-server/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
-    id("org.springframework.boot") version "2.2.4.RELEASE"
+    id("org.springframework.boot") version "2.3.3.RELEASE"
     id("org.jlleitschuh.gradle.ktlint")
 }
 
