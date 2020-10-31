d2d860e71e37d74b9ba4ca373eba45e3d6a4df74
joffrey-bion <joffrey.bion@gmail.com>
2020-10-31 13:38:45 +0100

    Upgrade Kotlin to 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    val kotlinVersion = "1.4.0"
+    val kotlinVersion = "1.4.10"
     kotlin("js") version kotlinVersion apply false
     kotlin("jvm") version kotlinVersion apply false
     kotlin("multiplatform") version kotlinVersion apply false
