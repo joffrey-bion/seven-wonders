ff992db4ec52331ceb753cae1e8b73fa90adb334
joffrey-bion <joffrey.bion@gmail.com>
2020-11-27 14:24:20 +0100

    Upgrade to Spring Boot 2.4.0

--- sw-server/build.gradle.kts
+++ sw-server/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
-    id("org.springframework.boot") version "2.3.3.RELEASE"
+    id("org.springframework.boot") version "2.4.0"
 }
 
 apply(plugin = "io.spring.dependency-management")
