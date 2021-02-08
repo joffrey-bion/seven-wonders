8ccccfe4e44ff69b3a68a40406a472cd3567838c
joffrey-bion <joffrey.bion@gmail.com>
2021-02-08 20:56:34 +0100

    Add Kotlinx Serialization converter for HTTP traffic (correctly)
    Resolves:
    https://github.com/joffrey-bion/seven-wonders/issues/87

--- sw-server/src/main/kotlin/org/luxons/sevenwonders/server/SevenWonders.kt
+++ sw-server/src/main/kotlin/org/luxons/sevenwonders/server/SevenWonders.kt
@@ -1,10 +1,19 @@
 package org.luxons.sevenwonders.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
 
 @SpringBootApplication
-class SevenWonders
+class SevenWonders {
+
+    @Bean
+    fun additionalConverters(): HttpMessageConverters? {
+        return HttpMessageConverters(KotlinSerializationJsonHttpMessageConverter())
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SevenWonders>(*args)
