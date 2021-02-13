c77fc93bb404b4af04e931a7672f97bf9f591911
joffrey-bion <joffrey.bion@gmail.com>
2021-02-13 19:19:55 +0100

    Add metrics labels for default Spring Boot grafana board
    https://grafana.com/grafana/dashboards/6756

--- sw-server/src/main/kotlin/org/luxons/sevenwonders/server/SevenWonders.kt
+++ sw-server/src/main/kotlin/org/luxons/sevenwonders/server/SevenWonders.kt
@@ -1,10 +1,13 @@
 package org.luxons.sevenwonders.server
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
+import java.net.InetAddress
 
 @SpringBootApplication
 class SevenWonders {
@@ -12,6 +15,13 @@ class SevenWonders {
     @Bean
     fun additionalConverters(): HttpMessageConverters? {
         return HttpMessageConverters(KotlinSerializationJsonHttpMessageConverter())
+    }
+
+    @Bean
+    fun metricsCommonTags(): MeterRegistryCustomizer<MeterRegistry>? = MeterRegistryCustomizer { registry ->
+        registry.config()
+            .commonTags("application", "SevenWonders")
+            .commonTags("instance", InetAddress.getLocalHost().hostAddress)
     }
 }
 
