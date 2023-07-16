8541232f12f03638ed0e96c848980da25cc8ccf3
Joffrey Bion <joffrey.bion@gmail.com>
2023-07-16 11:26:16 +0200

    Stop using deprecated mainOutputFile

--- sw-ui/build.gradle.kts
+++ sw-ui/build.gradle.kts
@@ -39,11 +39,11 @@ kotlin {
 tasks.named<ProcessResources>("jsProcessResources") {
     val webpack = project.tasks.withType(KotlinWebpack::class).first()
 
-    val bundleFile = webpack.outputFileName
+    val bundleFile = webpack.mainOutputFileName
     val publicPath = "./" // TODO get public path from webpack config
 
     filesMatching("*.html") {
-        expand("bundle" to bundleFile, "publicPath" to publicPath)
+        expand("bundle" to bundleFile.get(), "publicPath" to publicPath)
     }
 }
 
