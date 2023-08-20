f8cd8ad05d9ec0d39f2e24b43bea2aa27ad1e61a
Joffrey Bion <joffrey.bion@gmail.com>
2023-08-20 12:10:57 +0200

    Remove unnecessary github info from build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,28 +25,6 @@ gradleEnterprise {
 
         val isCIBuild = !System.getenv("CI").isNullOrEmpty()
         tag(if (isCIBuild) "CI" else "local")
-
-        val isGithubActionsBuild = !System.getenv("GITHUB_ACTIONS").isNullOrEmpty()
-        if (isGithubActionsBuild) {
-            addGithubActionsData()
-        }
-    }
-}
-
-fun BuildScanExtension.addGithubActionsData() {
-    value("GitHub Event", System.getenv("GITHUB_EVENT_NAME"))
-    value("GitHub Workflow", System.getenv("GITHUB_WORKFLOW"))
-    value("GitHub Run ID", System.getenv("GITHUB_RUN_ID"))
-    value("GitHub Run number", System.getenv("GITHUB_RUN_NUMBER"))
-    value("Commit", System.getenv("GITHUB_SHA"))
-
-    val ref = System.getenv("GITHUB_REF") ?: ""
-    val isTagBuild = ref.startsWith("refs/tags/")
-    if (isTagBuild) {
-        tag("tag")
-        value("Tag", ref.removePrefix("refs/tags/"))
-    } else {
-        value("Branch", ref.removePrefix("refs/heads/"))
     }
 }
 
