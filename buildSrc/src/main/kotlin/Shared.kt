1f3f1d72f415882ef03b13f0df3671ba1007179f
Joffrey Bion <joffrey.bion@gmail.com>
2025-11-23 23:59:41 +0100

    Update to JDK 21

--- buildSrc/src/main/kotlin/Shared.kt
+++ buildSrc/src/main/kotlin/Shared.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.*
 /**
  * The major JDK version shared between all toolchain settings.
  */
-const val JdkVersion = 17
+const val JdkVersion = 21
 
 /**
  * The compiler options shared by all Kotlin compilations in the project.
